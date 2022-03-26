5256a3aea28375437e2e707f6251ceba2bc83bb4
PoloClubeX <edwardlopez625@gmail.com>
2022-03-26 13:02:16 -0500

    Se crea el proyecto en spring y se configura el archivo pom.xml para la conexión a base de datos en localhost

---
+++ src/main/java/Parameta/com/ApiRestEmployees/ApiRestEmployeesApplication.java
@@ -0,0 +1,13 @@
+package Parameta.com.ApiRestEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Parameta/com/ApiRestEmployees/ApiRestEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package Parameta.com.ApiRestEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
